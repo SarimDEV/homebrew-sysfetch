0fee08eb82515fe6c3fefcef8dbce24d939172a2
Muhammad Sarim <m2sarim@uwaterloo.ca>
2023-09-18 01:26:20 -0400

    fix

--- Formula/sysfetch.rb
+++ Formula/sysfetch.rb
@@ -2,8 +2,8 @@ class Sysfetch < Formula
   desc "Explore your system and terminal environment with elegance."
   homepage "https://github.com/SarimDEV/sysfetch"
   url "https://github.com/SarimDEV/sysfetch/releases/latest/download/sysfetch-mac.tar.gz"
-  sha256 "ce2718cc0986694625fc33664a08a89fc031ad28e57d6c543d05c7f3fc046bfe"
-  version "0.1.0"
+  sha256 "ab4f397b05e1de2cc570ec7dd081a62b30c9276da6419031fd90b269275e610a"
+  version "0.1.1"
 
   def install
     bin.install "sysfetch"
